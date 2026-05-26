12672acabfe622eef258cc1cb914acecb686d61b
Rahul-pamula <rahulpamula123@gmail.com>
2026-05-26 15:44:16 +0530

    add new pattern programs: Butterfly and Diamond

--- patterns/Dimond.java
+++ patterns/Dimond.java
@@ -1,28 +1,23 @@
 import java.util.*;
-public class Solid_rhombus{
-    public static void main(String[] args){
+public class Dimond{
+    public static void main(String[] rahul){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=1;i<=n;i++){
+        for(int i =1;i<=n;i++){
             for(int j=1;j<=n-i;j++){
                 System.out.print(" ");
-
             }
-            for(int j=1;j<=n;j++){
+            for(int j =1;j<=i;j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
-        //with numbers
-            
-        System.out.println();
-         for(int i=1;i<=n;i++){
+        for(int i =n;i>=1;i--){
             for(int j=1;j<=n-i;j++){
-                System.out.print( " ");
-
+                System.out.print(" ");
             }
-            for(int j=1;j<=n;j++){
-                System.out.print(j + " ");
+            for(int j =1;j<=i;j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
