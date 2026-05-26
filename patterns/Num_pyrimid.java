f47710ffe26f62c4690630084e5991b40b60812e
Rahul-pamula <rahulpamula123@gmail.com>
2026-05-26 12:25:48 +0530

    add various pattern programs including half pyramid, inverted pyramid, and solid rectangle

---
+++ patterns/Num_pyrimid.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Num_pyrimid{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // outer loop 1 to n rows 
+        
+        for (int i =1;i<=n;i++){
+
+            //  inner loop for spaces n-i
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+
+            // inner loop for column num 1 to i
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ patterns/Outer_pyrimid.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Outer_pyrimid{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // for the outer pyrimid with solid base
+        // for(int i=1;i<=n;i++){
+        //     for(int s=1;s<=n-i;s++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=2*i-1;j++){
+        //         if(i==1||i==n||j==1||j==2*i-1){
+        //             System.out.print("*");
+        //         }
+        //         else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+
+
+       
+        // for the outer pyrimid with base is spaced
+
+        for(int i = 1; i <= n; i++) {
+
+            // spaces
+            for(int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+            // pattern
+            for(int j = 1; j <= 2*i - 1; j++) {
+
+                if(i == n) {
+                    // base row: alternate stars
+                    if(j % 2 != 0) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+
+                else if(i == 1 || j == 1 || j == 2*i - 1) {
+                    System.out.print("*");
+                }
+
+                else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
+    
