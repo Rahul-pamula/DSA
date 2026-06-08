4ea1c391353eda28d2239d53b37410a3e23eef9b
Rahul-pamula <rahulpamula123@gmail.com>
2026-06-08 22:41:20 +0530

    fix: remove redundant semicolon in bubble sort swap flag assignment

--- sorting/Bubble_sort.java
+++ sorting/Bubble_sort.java
@@ -14,7 +14,7 @@ public class Bubble_sort {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    flag = true;
+                    flag = true
                 }
             }
 
